6038f55148dafa57c16efb535934c77cce6e9672
Zep Fietje <hey@zepfietje.com>
2023-12-28 14:34:28 +0100

    Update icons

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -8,7 +8,6 @@ use App\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\MinimalTheme;
-use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\SpatieLaravelTranslatablePlugin;
