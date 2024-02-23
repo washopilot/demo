23c4aae2f01969540857b8e1387405707acf193d
Zep Fietje <hey@zepfietje.com>
2024-02-23 15:34:41 +0100

    Update icon

--- app/Filament/Clusters/Products.php
+++ app/Filament/Clusters/Products.php
@@ -6,7 +6,7 @@ use Filament\Clusters\Cluster;
 
 class Products extends Cluster
 {
-    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+    protected static ?string $navigationIcon = 'heroicon-m-squares-2x2';
 
     protected static ?string $navigationGroup = 'Shop';
 
